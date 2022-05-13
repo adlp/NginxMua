014f9865b89d7d6a5478503339b73005cfdc8067
Antoine Delaporte <antoine.delaporte@agoracalyce.com>
2022-05-13 21:09:48 +0200

    1st commit

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # NginxMua
+
+Take care :
+  * This an working example (in my context)
+  * This is my 1st version, with apparent password in log !!!!!!! (shoot it fast)
+  * This is the result of a lot of research, and no author are congratulated :_(  (I applogies)
+
+Subject:
+  * I need an pop3/imap reverse proxy
+  * I like perdition, but I love nginx
+
+Future:
+  * Integrate an soft "Fail2ban"
+  * Redirection to imap/pop3 honeypot
+  * Rewrite login by rules
+  * Verify with multiple imap server :DDD
+
+# How it's work ?
+## Imap/pop3
+  * The file concerned is conf.d/mail.conf
+  * It present the ssl certificate and capability for each protocol
+  * Do not use SMTP proxy of my config please
+  * The auth_http parameter call (localy) an url 
+
+## Http
+  * The file concerned is conf.d/http.conf
+  * This part create an http server with perl lib
+  * The files requested for auth are there : perl-lib
+
+## Files in perl-lib
+  * Their job is to receive the auth request, and answer an law ;)
+
+
+Okay this is an horriful doc, for an horriful config...
+But any help are accepted ;)
+
+
+Antoine.
---
+++ perl-lib/mailauth.pm
@@ -0,0 +1,113 @@
+package mailauth;
+use nginx;
+use Sys::Syslog qw(:standard :macros setlogsock);  # standard functions & macros
+use Data::Dumper;
+
+my $mapfile="/etc/nginx/perl/lib/dom2srv.txt";
+
+# https://www.nginx.com/resources/wiki/start/topics/examples/imapauthenticatewithapacheperlscript/
+#  password = URLDecoder.decode(password.replaceAll("\\+", "%2b"), "UTF-8");
+
+our $auth_ok;
+our $mail_server_ip={};
+our $protocol_ports={};
+$mail_server_ip->{'mailhost01'}="10.0.10.2";
+$mail_server_ip->{'mailhost02'}="10.0.12.23";
+$protocol_ports->{'pop3'}=110;
+$protocol_ports->{'imap'}=143;
+$protocol_ports->{'smtp'}=587;
+$protocol_ports->{'smtp'}=25;
+
+sub handler {
+  my $r = shift;
+  $auth_ok=1;
+
+  my $headers = $r;
+
+#http://nginx.org/en/docs/mail/ngx_mail_auth_http_module.html
+#Request:
+#    GET /auth HTTP/1.0
+#    Host: localhost
+#    Auth-Method: plain # plain/apop/cram-md5/external
+#    Auth-User: user
+#    Auth-Pass: password
+#    Auth-Protocol: imap # imap/pop3/smtp
+#    Auth-Login-Attempt: 1
+#    Client-IP: 192.0.2.42
+#    Client-Host: client.example.org
+#Good response:
+#    HTTP/1.0 200 OK
+#    Auth-Status: OK
+#    Auth-Server: 198.51.100.1
+#    Auth-Port: 143
+#Bad response:
+#    HTTP/1.0 200 OK
+#    Auth-Status: Invalid login or password
+#    Auth-Wait: 3
+
+
+  openlog("nginx-mua ======= ", 'noeol,nonul');
+  setlogsock({ type => "udp", host => "172.17.0.1", port => 514 });
+  setlogsock("udp", "172.17.0.1");
+
+  syslog('info', "Ca n'est que le debut");
+  open(FD,"<",$mapfile);
+
+  my %hash;
+  while(<FD>)
+  {
+    chomp;
+    my @cdc=split(/;/,$_);
+    #dom;            host;     smtp;pop;imap
+    $hash{$cdc[0]}{'host'}=$cdc[1];
+    $hash{$cdc[0]}{'smtp'}=587;
+    if(defined($cdc[2]) and $cdc[2] != "") { $hash{$cdc[0]}{'smtp'}=$cdc[2] }
+    $hash{$cdc[0]}{'pop3'}=110;
+    if(defined($cdc[3]) and $cdc[3] != "") { $hash{$cdc[0]}{'pop3'}=$cdc[3] }
+    $hash{$cdc[0]}{'imap'}=143;
+    if(defined($cdc[4]) and $cdc[4] != "") { $hash{$cdc[0]}{'imap'}=$cdc[4] }
+  }
+  close(FD);
+
+  if (crypt($r->header_in("Auth-Pass"), $hash->{'password'}) eq $r->header_in("Auth-Pass")){
+    syslog('info', "1er");
+    $auth_ok=1;
+    }
+
+  if ($auth_ok==1){
+    $r->header_out("Auth-Status", "OK") ;
+    my @emailcuted=split(/@/,$r->header_in('Auth-User'));
+    syslog('info', join(':',$r->header_in('Host'), $r->header_in('Auth-Method'), $r->header_in('Auth-User'),
+        $r->header_in('Client-IP'), $r->header_in('Auth-Protocol'), $r->header_in('Auth-Login-Attempt'),
+        $r->header_in('Auth-Pass'), $r->header_in('Client-Host')));
+    if(not defined($emailcuted[1])) {
+      syslog('info', "On reste sur le vieux");
+      #$r->header_out("Auth-Server", $mail_server_ip->{$hash->{'mail_server'}});
+      $r->header_out("Auth-Server", $mail_server_ip->{'mailhost01'});
+      #if ($r->header_in("Auth-Protocol") ne "smtp") {
+      $r->header_out("Auth-Port", $protocol_ports->{$r->header_in("Auth-Protocol")});
+      syslog('info',join(':',$mail_server_ip->{'mailhost01'},$protocol_ports->{$r->header_in("Auth-Protocol")}));
+      }
+    else {
+      syslog('info', "On change de serveur");
+      $hosteur='*';
+      if(defined($hash{$emailcuted[1]})) {
+        $hosteur=$emailcuted[1];
+        }
+      $r->header_out("Auth-Server", $hash{$hosteur}{'host'});
+      $r->header_out("Auth-Port",   $hash{$hosteur}{$r->header_in("Auth-Protocol")});
+      syslog('info', join(':',$hash{$hosteur}{'host'},$hash{$hosteur}{$r->header_in("Auth-Protocol")}));
+      }
+    }
+  else {
+    $r->header_out("Auth-Status", "Invalid login or password") ;
+  }
+
+  $r->send_http_header("text/html");
+
+
+  return OK;
+}
+
+1;
+__END__ 
